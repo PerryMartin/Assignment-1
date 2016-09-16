f5e1c274021a5f969d7173f62bb640089e2eb640
PerryMartin <rocky6117@live.com>
2016-09-16 16:16:57 -0400

    Update AssignmentOne.java

--- AssignmentOne.java
+++ AssignmentOne.java
@@ -8,17 +8,12 @@
  ****************************************************************************/
 import java.util.Scanner;
 class maxRun {
-       public static void main(String[] args) {
-
-       // input
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input a string : ");
-        String runString = input.nextLine();
-    
-    //Process
+       
+       	 //Process
     //http://stackoverflow.com/users/705175/eric
-
-    int maxRunCount = 0;
+       public static int CountingRun(String runString){
+   
+ 	int maxRunCount = 0;
     int stringCount = 1;
     char nextTime = '\u0000';  
            for (int run=0; run<runString.length(); run++) {
@@ -33,8 +28,21 @@ class maxRun {
     //reassign the previous before next iteration
     nextTime = runString.charAt(run);  
 }
-//output
-System.out.println("Max run is: " + maxRunCount);
-     }
+		
+		return maxRunCount;
+	}
+	
+	public static void main(String[] args){
+	// input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a string : ");
+        String output = input.nextLine();
+        
+    //ouput
+    
+    System.out.println("Max run is: " + CountingRun(output));
+   
+
+	}
 }
 
